ae46b9ce63b85f88114f7b6c3959d6d1f4c9a972
Eduardo Teixeira <edujunior_braga@hotmail.com>
2020-12-17 16:38:51 -0300

    projeto

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function validar(){
-alert("CEP VALIDO")
+
+var cep = document.getElementById("numero");
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function(){
+    if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("texto").innerHTML = cep;
+      this.responseText;
+    }
+}   
+
+xhttp.open("GET","https://cep.awesomeapi.com.br/json/09371110",true, )
+xhttp.send(JSON.stringify(
+    {
+"cep": "03375000",
+"address_type": "Rua",
+"address_name": "Hemisfério",
+"address": "Rua Hemisfério",
+"state": "SP",
+"district": "Chácara Belenzinho",
+"city": "São Paulo",
+"ddd": "11"
+}
 }
