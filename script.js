c3232d729f3d8ddf4af0413c01498221552321c8
Eduardo Teixeira <edujunior_braga@hotmail.com>
2020-12-18 17:15:51 -0300

    projeto

--- script.js
+++ script.js
@@ -12,13 +12,13 @@ xhttp.onreadystatechange = function(){
 xhttp.open("GET","https://cep.awesomeapi.com.br/json/09371110",true, )
 xhttp.send(JSON.stringify(
     {
-"cep": "03375000",
+"cep": "09371110",
 "address_type": "Rua",
-"address_name": "Hemisfério",
-"address": "Rua Hemisfério",
+"address_name": "Marechal Deodoro da Fonseca",
+"address": "Rua Marechal",
 "state": "SP",
-"district": "Chácara Belenzinho",
-"city": "São Paulo",
+"district": "Parque São Vicente",
+"city": "Mauá",
 "ddd": "11"
 }
 }
