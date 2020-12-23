fe90199d2f4e1e6cb2ab9bee5c1088e858b5674f
Eduardo Teixeira <edujunior_braga@hotmail.com>
2020-12-23 16:30:40 -0300

    finalizado

--- script.js
+++ script.js
@@ -3,25 +3,20 @@ function validar(){
 var cep = document.getElementById("numero");
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function(){
-    if (this.readyState == 4 && this.status == 200) {
-        var json = JSON.parse(this.responseText);
-            document.getElementById("texto").innerHTML = '<br>' + 'Endereço do CEP'+ '<ul>' +
-            '<li>' + json.cep + '</li>' +
-            '<li>' + json.address + '</li>' + 
-            '<li>' + json.address_type + '</li>' +
-            '<li>' + json.state + '</li>' +
-            '<li>' + json.district + '</li>' +
-            '<li>' + json.city + '</li>' +
-            '<li>' + json.ddd + '</li>' +
-            '</ul>'
-    }
+if (this.readyState == 4 && this.status == 200) {
+var json = JSON.parse(this.responseText);
+document.getElementById("texto").innerHTML = '<h2>' + 'Endereço do CEP'+ '</h2>' + '<ul>' +
+'<li>' + "CEP: " + json.cep + '</li>' +
+'<li>' + "Endereço: " + json.address + '</li>' + 
+'<li>' + "Tipo de endereço: " + json.address_type + '</li>' +
+'<li>' + "Estado: " + json.state + '</li>' +
+'<li>' + "Bairro: " + json.district + '</li>' +
+'<li>' + "Cidade: " + json.city + '</li>' +
+'<li>' + "DDD: " + json.ddd + '</li>' +
+'</ul>'
+}
 }   
-
+        
 xhttp.open("GET","https://cep.awesomeapi.com.br/json/" + cep.value,true,)
 xhttp.send();
-
-
-    }
-
-    //formatar o texto igual no pdf informado!
-    // ref do JSON linha 94 e 95 https://github.com/felipe199903/examples_validation_front/blob/master/js/script.js 
+}
