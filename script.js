ea3d715428ea3971e5875f4adb6dba1abb1823e5
Eduardo Teixeira <edujunior_braga@hotmail.com>
2021-01-13 22:19:22 -0300

    Projeto

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function(){
 if (this.readyState == 4 && this.status == 200) {
 var json = JSON.parse(this.responseText);
-document.getElementById("texto").innerHTML = '<h2>' + 'Endereço do CEP'+ '</h2>' + '<ul>' +
-'<li>' + "CEP: " + json.cep + '</li>' +
-'<li>' + "Endereço: " + json.address + '</li>' + 
-'<li>' + "Tipo de endereço: " + json.address_type + '</li>' +
-'<li>' + "Estado: " + json.state + '</li>' +
-'<li>' + "Bairro: " + json.district + '</li>' +
-'<li>' + "Cidade: " + json.city + '</li>' +
-'<li>' + "DDD: " + json.ddd + '</li>' +
+document.getElementById("texto").innerHTML = '<br>'+ '<h2>' + 'Endereço do CEP'+ '</h2>' + '<ul>' +
+'<li>' + "CEP: " + json.cep + '</li>' + '<br>' +
+'<li>' + "Endereço: " + json.address + '</li>' + '<br>' +
+'<li>' + "Tipo de endereço: " + json.address_type + '</li>' + '<br>' + 
+'<li>' + "Estado: " + json.state + '</li>' + '<br>' +
+'<li>' + "Bairro: " + json.district + '</li>' + '<br>' +
+'<li>' + "Cidade: " + json.city + '</li>' + '<br>' + 
+'<li>' + "DDD: " + json.ddd + '</li>' + '<br>' +
 '</ul>'
 }
 }   
